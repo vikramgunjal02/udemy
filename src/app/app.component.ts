f9d3e1ccac084c8287ac28866a38a1f93b079db9
vikram gunjal <vikramgunjal01@gmail.com>
2024-07-05 15:52:20 +0530

    Basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
-  title = 'udemy';
+  title = 'To-Do';
+  users = DUMMY_USERS
+  selectedUserId = 'u1'
+
+  get selectedUser(){
+    return this.users.find((ele)=>{
+      return ele.id === this.selectedUserId
+    })!
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output, } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input() id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  data = DUMMY_USERS
+
+  get imagePath() {
+    return 'assets/users/users' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
