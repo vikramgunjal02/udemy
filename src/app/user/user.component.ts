636e6a4bb74dc770e787873f5af3034557ee5be8
vikram gunjal <vikramgunjal01@gmail.com>
2024-07-05 16:21:32 +0530

    create the list as a dyanamic

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,18 @@
 import { Component, EventEmitter, Input, Output, } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
+// type User = {
+//   id:string;
+//   name:string;
+//   avatar:string;
+// }
+
+interface User {
+  id:string;
+  name:string;
+  avatar:string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,18 +21,16 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({required:true}) user!:User;
   @Output() select = new EventEmitter<string>();
 
   data = DUMMY_USERS
 
   get imagePath() {
-    return 'assets/users/users' + this.avatar;
+    return 'assets/users/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
