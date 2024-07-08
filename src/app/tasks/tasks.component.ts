27eda45a7d3ade9148e57869902fe803a49e6218
vikram gunjal <vikramgunjal01@gmail.com>
2024-07-08 10:14:36 +0530

    Added the form

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
     selector: 'app-tasks',
     standalone: true,
     templateUrl: './tasks.component.html',
     styleUrl: './tasks.component.css',
-    imports: [TaskComponent]
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
 @Input({required:true}) userId?:string;
 @Input({required:true}) name?:string;
+addTaskFlag:boolean=false;
 tasks =[{
   id:'t1',
   userId:'u1',
@@ -32,5 +34,12 @@ get selectedUserTasks(){
 
 onCompleteTask(id:string){
 this.tasks = this.tasks.filter((task) => task.id !==id)
+}
+
+addTask(){
+  this.addTaskFlag = true;
+}
+onCancelAddTask(){
+  this.addTaskFlag =false;
 }
 }
